a41cbba5acfd9ad68822b6da698cf9c63f51c845
Luis Berciano Suárez <luiisbercianoo@gmail.com>
2025-05-14 18:50:40 +0200

    feat(dungeon-generator): disable dungeon generator automatic start

---
+++ Assets/Scripts/Test/GenerateDungeonOnStart.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+/// <summary>
+/// Before, dungeon generator used to generate dungeons in its Start() method
+/// that changed in order to gain more control, and now generation is done by
+/// calling GenerateDungeon() method. The purpose of this script is to generate
+/// a dungeon in the dungeon generator test scene inmediately the play button
+/// is pressed.
+/// </summary>
+public class GenerateDungeonOnStart : MonoBehaviour
+{
+    [SerializeField] DungeonGenerator dungeonGenerator;
+    void Start()
+    {
+        dungeonGenerator.GenerateDungeon();        
+    }
+}
