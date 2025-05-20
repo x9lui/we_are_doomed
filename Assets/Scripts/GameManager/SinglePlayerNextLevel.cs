a9a29c901ec5277d047534d4e2a6ec8f6ef03710
Luis Berciano Suárez <luiisbercianoo@gmail.com>
2025-05-21 00:55:07 +0200

    fix(single-player-next-level): now the item action is not triggered multiple times

--- Assets/Scripts/GameManager/SinglePlayerNextLevel.cs
+++ Assets/Scripts/GameManager/SinglePlayerNextLevel.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class SinglePlayerNextLevel : MonoBehaviour
 {
+
+    /*We need this flag because the gameObject is not destroyed instantly
+    and that can lead to multiple calls to NextLevel().*/
+    private bool triggered = false;
     void OnTriggerEnter(Collider collider)
     {
+        if (triggered) return;
+        
         if (collider.gameObject.CompareTag("Player"))
         {
+            triggered = true;
             SinglePlayerGameManager.Instance.NextLevel();
             Destroy(gameObject);
         }
