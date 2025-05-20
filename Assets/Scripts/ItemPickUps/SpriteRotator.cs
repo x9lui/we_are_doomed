7c84524dc6b92da703539bb156bca3a1452af778
PaellaRosa <pepe.oterocas@alum.uca.es>
2025-05-20 21:29:21 +0200

    Cambio en el sprite rotator

--- Assets/Scripts/ItemPickUps/SpriteRotator.cs
+++ Assets/Scripts/ItemPickUps/SpriteRotator.cs
@@ -3,13 +3,26 @@ using UnityEngine;
 public class SpriteRotator : MonoBehaviour
 {
     private Transform target;
+
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        TryFindPlayer();
     }
 
     void Update()
     {
+        if (target == null)
+        {
+            TryFindPlayer();
+            if (target == null) return;
+        }
         transform.LookAt(target);
+    }
+
+    private void TryFindPlayer()
+    {
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+            target = playerObj.transform;
     }
 }
